4384df365ba02a622ac73101e8354d4b8e17a8bb
Syed Ishmam <syedishmam17@gmail.com>
2021-10-20 15:53:16 -0400

    product object strings instantiate

--- web/productobjects/WebLandingPage.java
+++ web/productobjects/WebLandingPage.java
@@ -4,8 +4,19 @@ import org.openqa.selenium.WebDriver;
 
 public class WebLandingPage extends WebBasePage {
 	
-	//Contains all options in navbar. Menu, Location, Delivery, etc
+	//Contains all options in navbar
 	String navbarXpaths = "//p[@class='global-nav__header']";
+	//Contains all category xpaths in menu. ex "Espresso & Coffee"
+	String menuCategoriesXpaths = "//button[@class='product-menu__category__list__item__button']";
+	
+	String espressoCoffeeMenuCategoryXpath;
+	String teasMoreMenuCategoryXpath;
+	String frozenDrinksMenuCategoryXpath;
+	String sandwichesMoreMenuCategoryXpath;
+	String snacksWrapsMenuCategoriesXpath;
+	String bagelsMuffinsMenuCategoryXpath;
+	String donutsMenuCategoryXpath;
+	String brewAtHomeMenuCategroyXpath;
 
 	public WebLandingPage(WebDriver driver) {
 		super(driver);
