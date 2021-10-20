a8daa16e4302ed28bf4e3a9f5af4c2e632fb77a8
Syed Ishmam <syedishmam17@gmail.com>
2021-10-20 15:39:50 -0400

    Add logic to WebBasePage

--- web/productobjects/WebBasePage.java
+++ web/productobjects/WebBasePage.java
@@ -1,5 +1,22 @@
 package productobjects;
 
-public class WebBasePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public abstract class WebBasePage {
+
+	WebDriver driver;
+	
+	public WebBasePage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	protected void input(String xpath, String query) {
+		driver.findElement(By.xpath(xpath)).sendKeys(query);
+	}
+	
+	protected void click(String xpath) {
+		driver.findElement(By.xpath(xpath)).click();
+	}
+	
 }
