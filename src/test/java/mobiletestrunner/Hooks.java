4cbcea40d415ebf24b92a0b8e20d52c8276cff4e
Syed Rahman <syedrahman@Syeds-MBP-2.lan>
2021-10-28 16:20:57 -0400

    revised path to dunkin apk

--- src/test/java/mobiletestrunner/Hooks.java
+++ src/test/java/mobiletestrunner/Hooks.java
@@ -18,7 +18,7 @@ public class Hooks {
 	@BeforeMethod(alwaysRun=true)
 	public void setup() throws MalformedURLException {
 
-		String path = "/Users/syedrahman/Desktop/workspace/appium/apk-files/dunkin.apk";
+		String path = System.getProperty("user.dir").concat("/src/test/resources/dunkin.apk");
 		String url = "http://127.0.0.1:4723/wd/hub";
 		
 		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
