a710fc2fd2b77ad12058530b0e3af3c578333b99
Syed Rahman <syedrahman@Syeds-MBP-2.lan>
2021-10-29 04:07:20 -0400

    progress

--- src/test/java/mobilepageobjects/LandingPage.java
+++ src/test/java/mobilepageobjects/LandingPage.java
@@ -3,9 +3,16 @@ package mobilepageobjects;
 import io.appium.java_client.AppiumDriver;
 
 public class LandingPage extends BasePage{
+	
+	private String acceptButton = "acceptButton";
 
 	public LandingPage(AppiumDriver driver) {
 		super(driver);
+	}
+	
+	public void startingFlow() {
+		sleep(3000);
+		click(acceptButton);
 	}
 
 }
--- src/test/java/mobiletestrunner/LandingPageTest.java
+++ src/test/java/mobiletestrunner/LandingPageTest.java
@@ -2,11 +2,15 @@ package mobiletestrunner;
 
 import org.testng.annotations.Test;
 
+import mobilepageobjects.LandingPage;
+
 public class LandingPageTest extends Hooks{
 	
 	@Test
 	public void Test1( ) {
 		
+		LandingPage landingPage = new LandingPage(driver);
+		landingPage.startingFlow();
 	}
 
 }
