76bcbb79a6de92368c19734ea72e6bb985970fb0
Syed Ishmam <syedishmam17@gmail.com>
2021-11-02 15:14:05 -0400

    Fill sign up form text fields

--- src/test/java/mobiletestrunner/SignUpTest.java
+++ src/test/java/mobiletestrunner/SignUpTest.java
@@ -3,14 +3,47 @@ package mobiletestrunner;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.offset.PointOption;
 import mobilepageobjects.LandingPage;
+import mobilepageobjects.SignUpFormPage;
 
 public class SignUpTest extends Hooks {
 	
-	@Test
-	public void signUpTest() {
+	@Test(dataProvider = "sign-up-creds")
+	public void signUpTest(
+				String firstName,
+				String lastName,
+				String email,
+				String password,
+				String mobile,
+				String zipCode
+			) throws InterruptedException {
+		Thread.sleep(3000);
 		LandingPage landing = new LandingPage(driver);
+		SignUpFormPage signUp = new SignUpFormPage(driver);
+		
+		String tosAcceptSelector = "new UiSelector().className(\"android.widget.Button\").text(\"ACCEPT\")";
+		driver.findElement(MobileBy.AndroidUIAutomator(tosAcceptSelector)).click();
+		
 		landing.click(landing.landingPageItems.get("menu button"));
+		String signUpSelector = "new UiSelector().className(\"android.widget.Button\").textContains(\"SIGN UP\")";
+		driver.findElement(MobileBy.AndroidUIAutomator(signUpSelector)).click();
+		
+		driver.findElement(MobileBy.AndroidUIAutomator(signUp.firstNameFieldSelector)).sendKeys(firstName);
+		driver.findElement(MobileBy.AndroidUIAutomator(signUp.lastNameFieldSelector)).sendKeys(lastName);
+		driver.findElement(MobileBy.AndroidUIAutomator(signUp.emailAddressFieldSelector)).sendKeys(email);
+		driver.findElement(MobileBy.AndroidUIAutomator(signUp.passwordFieldSelector)).sendKeys(password);
+		driver.findElement(MobileBy.AndroidUIAutomator(signUp.mobileNumberFieldSelector)).sendKeys(mobile);
+		driver.findElement(MobileBy.AndroidUIAutomator(signUp.zipCodeFieldSelector)).sendKeys(zipCode);
+		
+		driver.findElement(MobileBy.AndroidUIAutomator(signUp.checkBoxButtonSelector));
+		
+		TouchAction touchAction = new TouchAction(driver);
+		touchAction.press(PointOption.point(100, 100)).moveTo(PointOption.point(200, 200));
+		
+		
 	}
 	
 	@DataProvider(name = "sign-up-creds")
